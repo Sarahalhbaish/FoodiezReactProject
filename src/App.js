05a04e709209b61de288dc8af506d6298a72570b
Sarahalhbaish <sarah21995@live.com>
2024-12-15 19:24:05 +0300

    Created Done

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import RegisterPage from './Component/RegisterPage';
+
+function App() {
+  return (
+    <div className="App">
+     <RegisterPage/>
+    </div>
+  );
+}
+
+export default App;
