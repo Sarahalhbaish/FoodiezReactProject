fca8abaed4da4606bf14c543f27fe2587777caf8
Sarahalhbaish <sarah21995@live.com>
2024-12-15 19:46:53 +0300

    Done

---
+++ src/Component/Cetegories.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Cetegories = () => {
+  return (
+    <div>Cetegories</div>
+  )
+}
+
+export default Cetegories
--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import Nav from "./Component/Nav";
 import RegisterPage from "./Component/RegisterPage";
 import Ingredient from "./Component/Ingredient";
 import Home from "./Component/Home";
+import Cetegories from "./Component/Cetegories";
+import Recipes from "./Component/Recipes";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
@@ -36,6 +38,24 @@ const router = createBrowserRouter([
       <div>
         <Nav />
         <Ingredient />
+      </div>
+    ),
+  },
+  {
+    path: "/Cetegories",
+    element: (
+      <div>
+        <Nav />
+        <Cetegories />
+      </div>
+    ),
+  },
+  {
+    path: "/Recipes",
+    element: (
+      <div>
+        <Nav />
+        <Recipes />
       </div>
     ),
   },
